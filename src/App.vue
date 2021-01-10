0fbcd3a17742c351438a82a8eaa5ae25212dce3e
imcris <zanardy20@hotmail.com>
2021-01-10 15:31:02 -0500

    init proyect whit base dashBoard

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <the-app-bar/>
+    <the-drawer/>
+    <the-settings/>
+    <the-view/>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+/* import {AppBar, Drawer, Footer,Settings, View } from '@/components/base'; */
+import TheAppBar from '@/components/base/TheAppBar';
+import TheDrawer from '@/components/base/TheDrawer';
+
+import TheSettings from '@/components/base/TheSettings';
+import TheView from '@/components/base/TheView';
+
+
+export default {
+  components:{TheAppBar, TheDrawer,TheSettings, TheView, }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/base/TheView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-main>
+    <router-view />
+
+    <dashboard-core-footer />
+  </v-main>
+</template>
+
+<script>
+import TheFooter from '@/components/base/TheFooter';
+  export default {
+    name: 'DashboardCoreView',
+
+    components: {
+      DashboardCoreFooter: TheFooter,
+    },
+  }
+</script>
