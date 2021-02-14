fe43e019f415c2fee330473f42c2054045794f76
imcris <zanardy20@hotmail.com>
2021-02-13 22:38:02 -0500

    config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-/*   publicPath: process.env.NODE_ENV === '/' */
+ /*  publicPath: process.env.NODE_ENV === '/' */
     
 }
+module.exports = { baseUrl: '/ImCrisam.github.io/' }
 module.exports = {
   "transpileDependencies": [
     "vuetify"
